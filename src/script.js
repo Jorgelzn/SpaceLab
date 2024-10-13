21afc89de65cd33863b6bed4695b83687e0b406d
Jorgelzn <jorgelizgo@gmail.com>
2024-10-13 22:37:12 +0200

    book of shaders

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import vertexShader from './shaders/vertexShader.glsl';
 
+import vertexShader from './shaders/vertexShader.glsl';
 import fragmentShader from './shaders/fragmentShader.glsl';
 
 // Set up
@@ -24,17 +24,20 @@ camera.position.z = 5; //Position the camera
 // Create a cube
 const geometry = new THREE.BoxGeometry();
 //const material = new THREE.MeshBasicMaterial({ color: 0xb3d9ff });ç
-const customShaderMaterial = new THREE.ShaderMaterial(vertexShader,fragmentShader)
+uniforms = {
+    u_time: { type: "f", value: 1.0 },
+    u_resolution: { type: "v2", value: new THREE.Vector2(renderer.domElement.width,renderer.domElement.height) },
+    u_mouse: { type: "v2", value: new THREE.Vector2() }
+};
+
+var customShaderMaterial = new THREE.ShaderMaterial({
+    uniforms : uniforms,
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader
+})
 //material.wireframe = true;
 const cube = new THREE.Mesh(geometry, customShaderMaterial);
 scene.add(cube);
 
 
-// Render the scene
-const animate = () => {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-};
-animate();
+renderer.render(scene, camera);
